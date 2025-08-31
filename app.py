e3b07af30005c22498ca5a60f7bd19faeb850d34
Aly J. <aly.jiwani009@gmail.com>
2025-08-31 16:01:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,8 @@ from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-gameweeks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
-             29, 30, 31, 32, 33, 34, 35, 36, 37, 38]
-false_id = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+gameweeks = list(range(39))
+false_id = [0] * 39
 
 bg_colour = '#121212'
 font_colour = '#FFFFFF'
